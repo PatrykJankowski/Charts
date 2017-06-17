e83d81e35ac8246f89daf731cacfa6e089381985
PatrykJankowski <patryk.k.jankowski@gmail.com>
2017-06-17 17:00:27 +0200

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { isNumeric } from 'rxjs/util/isNumeric';
-import {showWarningOnce} from "tslint/lib/error";
 
 @Component({
   selector: 'app-root',
@@ -10,13 +9,8 @@ import {showWarningOnce} from "tslint/lib/error";
 })
 export class AppComponent {
 
-  priority01 = [];
-  priority23 = [];
-  priority45 = [];
-
-  temp = '';
-  shown: string = '';
-  show: boolean = false;
+  csvData: any[] = [];
+  priority: any[] = [[], [], []];
 
   public chartData: any[] = [
     {data: 0, label: 'P0, P1'},
@@ -45,12 +39,15 @@ export class AppComponent {
     }
   };
 
-  csvData: any[] = [];
+  filter = '';
+  shown = '';
+  show = false;
 
   constructor (private http: Http) {}
 
+
   readCsvData () {
-    this.http.get('assets/sample.csv')
+    this.http.get('assets/data.csv')
       .subscribe(
         data => this.extractData(data),
         err => console.log(err)
@@ -58,20 +55,13 @@ export class AppComponent {
   }
 
 
-  //sprawdzac z pliku z danymi
-  checkGroup(member) {
-    if (member == 'TeamMember1' || member == 'TeamMember2' || member == 'TeamMember3' || member == 'TeamMember4') return 1;
-    else if (member == 'TeamMember5' || member == 'TeamMember6' || member == 'TeamMember7') return 2;
-    else return 3;
-  }
-
   private extractData(res: Response) {
     let csvData = res['_body'] || '';
     let allTextLines = csvData.split(/\r\n|\n/);
     let headers = allTextLines[0].split(';');
     let lines = [];
 
-    for ( let i = 0; i < allTextLines.length; i++) {
+    for (let i = 0; i < allTextLines.length; i++) {
       let data = allTextLines[i].split(';');
       if (data.length == headers.length) {
         let tarr = [];
@@ -82,30 +72,20 @@ export class AppComponent {
       }
     }
     this.csvData = lines;
-    this.filter('Original');
+    this.chartFilter('Original');
   }
 
-  public chartClicked(e: any): void {
-    console.log(e);
-  }
 
-  showData() {
-    this.show ? this.show = false : this.show = true;
-  }
-
-  public filter(filter) {
+  public chartFilter(filter) {
 
     let estimate01 = 0;
     let estimate23 = 0;
     let estimate45 = 0;
 
-    this.priority01 = [];
-    this.priority23 = [];
-    this.priority45 = [];
-
+    this.priority = [[],[],[]];
 
     if (filter != 'Groups')
-      this.temp = filter;
+      this.filter = filter;
     else if (this.chartLabels.length > 1)
       this.chartLabels.pop();
     else {
@@ -113,7 +93,7 @@ export class AppComponent {
       this.chartLabels.unshift('Group 1');
     }
 
-    if (this.temp == 'Original') {
+    if (this.filter == 'Original') {
       for (let i = 1; i <= 3; i++) {
         for (let entry of this.csvData) {
           if ((entry[5] == 'P0' || entry[5] == 'P1') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i)
@@ -123,34 +103,46 @@ export class AppComponent {
           else if ((entry[5] == 'P4' || entry[5] == 'P5') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i)
             estimate45 += parseInt(entry[6]);
         }
-        this.priority01.push(estimate01);
-        this.priority23.push(estimate23);
-        this.priority45.push(estimate45);
+        this.priority[0].push(estimate01);
+        this.priority[1].push(estimate23);
+        this.priority[2].push(estimate45);
 
       }
-    } else if (this.temp == 'Resolved' || this.temp == 'Unresolved') {
+    } else if (this.filter == 'Resolved' || this.filter == 'Unresolved') {
       for (let i = 1; i <= 3; i++) {
-
         for (let entry of this.csvData) {
-          if ((entry[5] == 'P0' || entry[5] == 'P1') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.temp)
+          if ((entry[5] == 'P0' || entry[5] == 'P1') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.filter)
             estimate01 += parseInt(entry[6]);
-          else if ((entry[5] == 'P2' || entry[5] == 'P3') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.temp)
+          else if ((entry[5] == 'P2' || entry[5] == 'P3') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.filter)
             estimate23 += parseInt(entry[6]);
-          else if ((entry[5] == 'P4' || entry[5] == 'P5') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.temp)
+          else if ((entry[5] == 'P4' || entry[5] == 'P5') && isNumeric(parseInt(entry[6])) && this.checkGroup(entry[4]) == i && entry[2] == this.filter)
             estimate45 += parseInt(entry[6]);
         }
-        this.priority01.push(estimate01);
-        this.priority23.push(estimate23);
-        this.priority45.push(estimate45);
+        this.priority[0].push(estimate01);
+        this.priority[1].push(estimate23);
+        this.priority[2].push(estimate45);
       }
     }
 
     this.chartData = [
-      {data: this.priority01, label: 'P0, P1', stack: 'test1'},
-      {data: this.priority23, label: 'P2, P3', stack: 'test2'},
-      {data: this.priority45, label: 'P4, P5', stack: 'test3'}
+      {data: this.priority[0], label: 'P0, P1', stack: 'test1'},
+      {data: this.priority[1], label: 'P2, P3', stack: 'test2'},
+      {data: this.priority[2], label: 'P4, P5', stack: 'test3'}
     ];
 
   }
+
+
+  checkGroup(member) {
+    if (member == 'TeamMember1' || member == 'TeamMember2' || member == 'TeamMember3' || member == 'TeamMember4') return 1;
+    else if (member == 'TeamMember5' || member == 'TeamMember6' || member == 'TeamMember7') return 2;
+    else return 3;
+  }
+
+
+  showData() {
+    this.show ? this.show = false : this.show = true;
+  }
+
 
 }
